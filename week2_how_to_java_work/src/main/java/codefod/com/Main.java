deed8ba1ecbda3cc65bc7fa939ed8a7ab03fd3a5
nguyenhoangthaituandev <nguyenhoangthaituandev@gmail.com>
2025-02-26 15:08:01 +0700

    [update] assigment 2

--- week2_how_to_java_work/src/main/java/codefod/com/Main.java
+++ week2_how_to_java_work/src/main/java/codefod/com/Main.java
@@ -6,5 +6,7 @@ public class Main {
         // TODO: Viết một chương trình Java cơ bản và thực chạy compile file chương trình ra bytecode
         // TODO: Thực chạy file bytecode bằng lệnh java
         // TODO: Thực hiện chạy file bytecode với cấu hình heap size, gc, ...
+
+        System.out.println("Hello World");
     }
 }
